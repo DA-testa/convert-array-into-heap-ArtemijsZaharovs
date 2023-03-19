18282cce6d0fc098ac462a38f0c9dd4fb3b8644b
ArtemijsZaharovs <124812244+ArtemijsZaharovs@users.noreply.github.com>
2023-03-19 13:29:13 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,39 +1,37 @@
-
-def sif(data, swap, i):
-    k = len(data)
+//221RDC035, Artemijs Zaharovs
+def sif (data, i, swaps):
+    n = len(data)
     index = i
-
-    l = 2 * i + 1
-    if l < k and data[l] < data[index]:
+    l = 2*i + 1
+    if l < n and data[l] < data[index]:
         index = l
-
-    r = 2 * i + 2
-    if r < k and data[r] < data[index]:
+    r = 2*i + 2
+    if r < n and data[r] < data[index]:
         index = r
-
     if i != index:
-        swap.append((i, index))
         data[i], data[index] = data[index], data[i]
-        sif(data, swap, index)
+        swaps.append((i, index))
+        sif(data, index, swaps)
+
 
 def build_heap(data):
-    swap = []
-    k = len(data)
+    
+    n = len(data)
+    swaps = []
+    for i in range(n//2, -1, -1):
+        sif(data, i, swaps)
+    return swaps
 
-    for i in range(n // 2, -1, -1):
-        sif(data, swap, i)
-
-    return swap
 
 def main():
-    k = int(input())
+
+    n = int(input())
     data = list(map(int, input().split()))
-
-    swap = build_heap(data)
-
-    print(len(swap))
-    for i, j in swap:
+    swaps = build_heap(data)
+    print(len(swaps))
+    for i, j in swaps:
         print(i, j)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
