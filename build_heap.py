3c4e163359a7e8da740351f166d987dc137d176e
ArtemijsZaharovs <124812244+ArtemijsZaharovs@users.noreply.github.com>
2023-03-19 12:40:00 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,39 @@
-# python3
 
+def sif(data, swap, i):
+    k = len(data)
+    index = i
+
+    l = 2 * i + 1
+    if l < k and data[l] < data[index]:
+        index = l
+
+    r = 2 * i + 2
+    if r < k and data[r] < data[index]:
+        index = r
+
+    if i != index:
+        swap.append((i, index))
+        data[i], data[index] = data[index], data[i]
+        sif(data, swap, index)
 
 def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    swap = []
+    k = len(data)
 
+    for i in range(n // 2, -1, -1):
+        sif(data, swap, i)
 
-    return swaps
-
+    return swap
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
+    k = int(input())
     data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
+    swap = build_heap(data)
 
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
+    print(len(swap))
+    for i, j in swap:
         print(i, j)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
